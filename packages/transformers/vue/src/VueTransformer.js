5e07a6bb591a9142b8ae902c538497b1cf3c919e
Dario Fagotto <dario.fagotto@gmail.com>
2020-03-13 16:39:31 +0100

    Generate postProcess test
    Not working, AST is removed before postProcess???

--- packages/transformers/vue/src/VueTransformer.js
+++ packages/transformers/vue/src/VueTransformer.js
@@ -8,6 +8,7 @@ import {
   compileStyle,
 } from '@vue/component-compiler-utils';
 import SourceMap from '@parcel/source-map';
+import generateBundleReport from '@parcel/utils/lib/generateBundleReport';
 //import vueTemplateCompiler from "vue-template-compiler";
 const vueTemplateCompiler = require('vue-template-compiler');
 // Should be devDep and dynamically loaded? how?
@@ -38,13 +39,13 @@ export default new Transformer({
     return config;
   },
 
-  async transform({asset, options, logger}) {
-    const id = md5FromObject({
+  async parse({asset, options, logger}) {
+    const vueId = md5FromObject({
       file: asset.filePath,
       id: asset.id,
       key: asset.uniqueKey,
     }).slice(-6);
-    const scopeId = `data-v-scope-${id}`; // If not scoped flag sould check! no need for null...
+    const scopeId = `data-v-scope-${vueId}`; // If not scoped flag sould check! no need for null...
 
     const code = await asset.getCode();
 
@@ -53,15 +54,11 @@ export default new Transformer({
       throw new Error('Invalid vue-template-compiler!!!');
     }
 
-    const parseOpts = {
-      filename: asset.filePath,
-      compiler: vueTemplateCompiler,
-    };
-
     const sfcDesc = parse({
       source: code,
       compiler: vueTemplateCompiler,
-      ...parseOpts,
+      filename: asset.filePath,
+      compiler: vueTemplateCompiler,
       sourceRoot: '',
       needMap: options.sourceMaps,
       copmilerOptions: {
@@ -70,21 +67,28 @@ export default new Transformer({
     });
     failOnError(sfcDesc, logger);
 
-    logger.warn(
-      sfcDesc.customBlocks.map(customBlock => {
-        return {message: `Unknown block found: "${customBlock.type}"`};
-      }),
-    );
+    return {
+      vueId,
+      scopeId,
+      sfcDesc,
+    };
+  },
 
-    let blocks = [];
+  async generate({asset}) {
+    return {code: await asset.getCode(), map: await asset.getMap()};
+  },
+
+  async transform({asset, logger}) {
+    const {vueId, scopeId, sfcDesc} = asset.ast;
+
+    asset.ast.scopedStyle = [];
+
+    let parts = [];
 
     // TODO: production???
 
     if (sfcDesc.script !== null) {
-      //logger.log({message: 'Found script'});
-      // TODO: ? script.content is the js
-      // Maybe same block as template???
-      blocks.push({
+      parts.push({
         type: sfcDesc.script.lang || 'js',
         code: sfcDesc.script.content,
         map: sfcDesc.script.map,
@@ -92,27 +96,27 @@ export default new Transformer({
     }
     if (sfcDesc.template !== null) {
       // TODO: functional???
-      //logger.log({message: 'Found template'});
 
       const isFunctional = sfcDesc.template.attrs.functional || false;
       const template = compileTemplate({
         source: sfcDesc.template.content,
         lang: sfcDesc.template.lang || 'html',
         isFunctional: isFunctional,
-        ...parseOpts,
+        filename: asset.filePath,
+        compiler: vueTemplateCompiler,
       });
       failOnError(template, logger);
 
-      blocks.push({
+      parts.push({
         type: 'js',
         code: `
-var $${id} = exports.default || module.exports;
-if(typeof $${id} === 'function') {
-  $${id} = $${id}.options;
+var $${vueId} = exports.default || module.exports;
+if(typeof $${vueId} === 'function') {
+  $${vueId} = $${vueId}.options;
 }
 
 /* template */
-Object.assign($${id}, (function () {
+Object.assign($${vueId}, (function () {
   ${template.code}
   return {
     render: render,
@@ -127,91 +131,95 @@ Object.assign($${id}, (function () {
     }
     for (const style of sfcDesc.styles) {
       // TODO: CSS modules???
-      //logger.log({message: 'Found style'});
-
-      if (style.scoped) {
-        // Should "compile" also non scoped?
-        //logger.log({message: 'Scoped style'});
-
-        const compiledStyle = compileStyle({
-          source: style.content,
-          preprocessLang: style.lang || 'css', // Only css? use other preprocs?
-          // Compile stylesheet in post to allow any preproc (should use AST and generate...)
-          // WARNING: if lang is not known in Vue it is treated as css without errors
-          scoped: style.scoped || false, // Alwais true in this case...
-          id: scopeId,
-          ...parseOpts,
-        });
-        failOnError(compiledStyle, logger);
-
-        blocks.push({
-          type: 'css', // Already processed by vue
-          code: compiledStyle.code,
-          map: compiledStyle.map,
-        });
-      } else {
-        blocks.push({
-          type: style.lang || 'css',
-          code: style.content,
-          map: style.map,
-        });
-      }
+      asset.ast.scopedStyle.push(style.scoped || false);
+      parts.push({
+        type: style.lang || 'css',
+        code: style.content,
+        map: style.map,
+        ast: {
+          scoped: style.scoped || false,
+        },
+      });
     }
     // TODO: HMR???
 
-    //logger.log({message: 'Result: ' + JSON.stringify(blocks)});
-
-    return blocks;
+    return parts;
   },
 
   async postProcess({assets, logger}) {
-    // Merge js assets in same module
     const results = [];
+
+    // Merge js assets in same module (template and code)
     let js = '';
     const map = new SourceMap();
     let deps = [];
+
+    // Style counter
+    let styleIdx = 0;
+
     for (const asset of assets) {
-      if (asset.type !== 'js') {
-        results.push({
-          // Like dependency? need copy?
-          type: asset.type,
-          code: await asset.getCode(),
-          map: await asset.getMap(),
-        });
-      } else {
-        const baseColumn = js.length;
-        js += await asset.getCode();
-        const cmap = await asset.getMap();
-        if (cmap) {
-          //logger.error({ message: JSON.stringify(cmap) });
-          map.addMap(cmap, 0, baseColumn);
-        }
-        for (const dep of asset.getDependencies()) {
-          deps.push({
-            // Dependency is not populated unless copied?
-            moduleSpecifier: dep.moduleSpecifier,
-            isAsync: dep.isAsync,
-            isEntry: dep.isEntry,
-            isOptional: dep.isOptional,
-            isURL: dep.isURL,
-            isWeak: dep.isWeak,
-            loc: dep.loc,
-            env: dep.env,
-            meta: dep.meta,
-            target: dep.target,
-            symbols: dep.symbols,
+      logger.warn({message: JSON.stringify(asset.ast)});
+      switch (asset.type) {
+        case 'js':
+          const baseColumn = js.length;
+          js += await asset.getCode();
+          const cmap = await asset.getMap();
+          if (cmap) {
+            map.addMap(cmap, 0, baseColumn);
+          }
+          for (const dep of asset.getDependencies()) {
+            deps.push({
+              // Dependency is not populated unless copied?
+              moduleSpecifier: dep.moduleSpecifier,
+              isAsync: dep.isAsync,
+              isEntry: dep.isEntry,
+              isOptional: dep.isOptional,
+              isURL: dep.isURL,
+              isWeak: dep.isWeak,
+              loc: dep.loc,
+              env: dep.env,
+              //meta: dep.meta, // Should drop meta???
+              target: dep.target,
+              symbols: dep.symbols,
+            });
+          }
+          break;
+        case 'css':
+          if (asset.ast.scopedStyle[styleIdx++]) {
+            const compiledStyle = compileStyle({
+              source: await asset.getCode(),
+              scoped: true,
+              id: asset.ast.scopeId,
+              filename: asset.filePath,
+              compiler: vueTemplateCompiler,
+            });
+            failOnError(compiledStyle, logger);
+
+            blocks.push({
+              type: 'css',
+              code: compiledStyle.code,
+              map: compiledStyle.map,
+            });
+            break;
+          }
+        // Volountary fall-trough
+        default:
+          results.push({
+            // Like dependency? need copy?
+            type: asset.type,
+            code: await asset.getCode(),
+            map: await asset.getMap(),
           });
-        }
       }
     }
-    //logger.warn({message: JSON.stringify(deps)});
+
     results.push({
       type: 'js',
       code: js,
       map: map,
       dependencies: deps,
     });
-    //logger.error({ message: JSON.stringify(results) });
+
     return results;
   },
 });
